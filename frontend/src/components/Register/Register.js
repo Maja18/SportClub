de9c8e750f7d9d6c53ee46d711b5ba8e441eb308
Maja Dragojlovic <maja98dragojlovic@gmail.com>
2022-11-22 18:34:09 +0100

    [FeatureUpdate] added registration form

--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import {
+    Button,
+    Form,
+    FormGroup,
+    Input,
+    Label
+  } from 'reactstrap';
+import './Register.css';
 
 const Register = () => {
     return(
-        <div>
-            <h1>Success</h1>
+        <div className="Register">
+            <h2>Register</h2>
+            <Form>
+                <FormGroup>
+                    <Label for="exampleEmail">Name</Label>
+                    <Input
+                    type="name"
+                    name="firstName"
+                    id="exampleName"
+                    placeholder="Name"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="exampleEmail">Last name</Label>
+                    <Input
+                    type="name"
+                    name="lastName"
+                    id="exampleLastName"
+                    placeholder="Last name"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="exampleEmail">Email</Label>
+                    <Input
+                    type="email"
+                    name="email"
+                    id="exampleEmail"
+                    placeholder="example@example.com"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="examplePassword">Password</Label>
+                    <Input
+                    type="password"
+                    name="password"
+                    id="examplePassword"
+                    placeholder="********"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <input type="radio" value="EDITOR" name="role" /> EDITOR
+                    <input type="radio" value="VIEWER" name="role" /> VIEWER
+                </FormGroup>
+                <Button color="success">Submit</Button>
+            </Form>
         </div>
     );
 
