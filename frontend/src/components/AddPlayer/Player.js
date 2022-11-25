3346cec5924c5f0a3c403632daef479a399f6e2a
Maja Dragojlovic <maja98dragojlovic@gmail.com>
2022-11-25 21:51:20 +0100

    [FeatureUpdate] uploading player picture

--- frontend/src/components/AddPlayer/Player.js
+++ frontend/src/components/AddPlayer/Player.js
@@ -14,12 +14,16 @@ import {
 const Player = () => {
     const [enteredName, setEnteredName] = useState('');
     const [enteredSalary, setEnteredSalary] = useState('');
+    const [fileName, setFileName] = useState('');
+    const [selectedFiles, setSelectedFiles] = useState(undefined);
+    const [currentFile, setCurrentFile] = useState(undefined);
     const navigateTo = useNavigate();
 
     const addPlayer = () => {
         const data = {
             playerName: enteredName,
-            salary: enteredSalary
+            salary: enteredSalary,
+            image: fileName
         }
 
         let token = localStorage.getItem('token').substring(1, localStorage.getItem('token').length-1);
@@ -36,6 +40,41 @@ const Player = () => {
                 alert("Please enter valid data!");
                 console.log(response);
             }); 
+    };
+
+    const selectFile = (event) => {
+        setSelectedFiles(event.target.files);
+    };
+
+    const uploadImage = () => {
+        let currentFile = selectedFiles[0];
+        setCurrentFile(currentFile);
+    
+        upload(currentFile)
+          .catch(() => {
+            setCurrentFile(undefined);
+          });
+          setSelectedFiles(undefined);
+    };
+
+    const upload = (file) => {
+        let formData = new FormData();
+      
+        formData.append("file", file);
+        let token = localStorage.getItem('token').substring(1, localStorage.getItem('token').length-1);
+      
+        return axios.post("http://localhost:8080/api/player/saveImage", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            'Authorization': 'Bearer ' + token
+          }  
+        }).then(response => {
+            setFileName(response.data)
+        })
+        .catch(response => {
+            console.log(response.data)
+            alert("Something went wrong with uploading image")
+        });  
     };
 
     return(
@@ -68,6 +107,20 @@ const Player = () => {
                         onChange={event => {
                             setEnteredSalary(event.target.value)
                         }}/> 
+                    <Label for="exampleEmail">Choose profile picture</Label>
+                        <Input
+                        type="file"
+                        name="image" 
+                        accept="image/png, image/jpeg"
+                        id="file" 
+                        onChange={selectFile}
+                    />
+                    <Button style={{marginTop:'10px'}}
+                        className="btn btn-success"
+                        disabled={!selectedFiles}
+                        onClick={uploadImage}> Upload
+                    </Button>
+                    
                     <div class="button-container-div">
                         <Button style={{marginTop:'30px', width:'100px'}} color="success" onClick={addPlayer} >Add</Button>
                     </div>
--- sportClub/src/main/java/com/sportClub/sportClub/controller/PlayerController.java
+++ sportClub/src/main/java/com/sportClub/sportClub/controller/PlayerController.java
@@ -6,13 +6,18 @@ import com.sportClub.sportClub.model.Person;
 import com.sportClub.sportClub.model.Player;
 import com.sportClub.sportClub.service.PersonServiceImpl;
 import com.sportClub.sportClub.service.PlayerServiceImpl;
+import com.sportClub.sportClub.utils.MediaUpload;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,10 +25,9 @@ import java.util.List;
 @RequiredArgsConstructor
 @SecurityRequirement(name = "javainuseapi")
 public class PlayerController {
-
-    private PersonServiceImpl personService;
-
     private final PlayerServiceImpl playerService;
+
+    private static String uploadDir = "user-photos";
 
     @GetMapping("/{player-id}")
     @PreAuthorize("hasAnyAuthority({'ROLE_EDITOR','ROLE_VIEWER'})")
@@ -79,6 +83,15 @@ public class PlayerController {
 
         return players == null ?
                 new ResponseEntity<>(HttpStatus.NOT_FOUND) : ResponseEntity.ok(players);
+    }
+
+    @PostMapping(value="/saveImage", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PreAuthorize("hasAuthority('ROLE_EDITOR')")
+    public String saveImageForRequest(@RequestParam("file") MultipartFile multipartFile ) throws IOException {
+
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename().replaceAll("\\s", ""));
+        MediaUpload.saveFile(uploadDir, fileName, multipartFile);
+        return fileName;
     }
 
 }
