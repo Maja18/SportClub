b58c7211eb3972135d3cd11199d5496c90ad4e61
Maja Dragojlovic <maja98dragojlovic@gmail.com>
2022-11-30 18:47:13 +0100

    [FeatureUpdate] added skills in database

--- sportClub/src/main/resources/import.sql
+++ sportClub/src/main/resources/import.sql
@@ -1,4 +1,6 @@
 INSERT INTO AUTHORITY (id ,name) VALUES (1, 'ROLE_EDITOR');
 INSERT INTO AUTHORITY (id ,name) VALUES (2, 'ROLE_VIEWER');
-
-
+INSERT INTO SKILL (id,description, name) VALUES  (10,'fast running','running');
+INSERT INTO SKILL (id,description, name) VALUES  (11,'good throwing','throwing');
+INSERT INTO SKILL (id,description, name) VALUES  (12,'good balancing','balancing');
+INSERT INTO SKILL (id,description, name) VALUES  (13,'good swimming','swimming');
